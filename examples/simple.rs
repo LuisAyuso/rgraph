12fa35bcef30a7993852fb2b1665fb9ace7b95ce
Luis Ayuso <luis.f.ayuso@gmail.com>
2017-12-08 12:08:51 +0100

    change naming of assets, not quite happy as I want to get rid of those spaces

--- examples/simple.rs
+++ examples/simple.rs
@@ -8,57 +8,56 @@ fn main(){
         let mut g = Graph::new();
 
         g.add_node(create_node!(
-                name: "gen one",
-                in: (),
-                out: (one: u32) {
+                gen_one () -> (one: u32) {
                     println!("              gen one");
                     one = 1u32;
                 }
             ));
 
         g.add_node(create_node!(
-                name: "plus one",
-                in: (one: u32),
-                out: (plusone : u32) {
+                plus_one (one: u32) -> (plusone : u32) {
                     println!("              plusone");
                     plusone = one + 1u32;
                 }
             ));
 
         g.add_node(create_node!(
-                name: "the one task",
-                in: (one: u32, plusone : u32),
-                out: (last_value: f32) {
+                the_one_task  (one: u32, plusone : u32) ->
+                              (last_value: f32) {
                     println!("              the one task");
                     last_value = (one + plusone) as f32;
                 }
             ));
 
         g.add_node(create_node!(
-                name: "list",
-                in: (list : Vec<u32>),
-                out: () {
+                list (list : Vec<u32>) -> () {
                     println!("             list");
                 }
             ));
 
-        let mut cache = Cache::new();
+        g.bind_asset("gen_one :: one", "plus_one :: one").expect("binding must be doable");
+        g.bind_asset("gen_one :: one", "the_one_task :: one").expect("binding must be doable");
+        g.bind_asset("plus_one :: plusone", "the_one_task :: plusone").expect("binding must be doable");
+
+        let mut cache = ValuesCache::new();
 
         for _ in 0..10 {
 
             {
                 let mut solver = GraphSolver::new(&g, &mut cache);
                 assert!(solver.execute("nop").is_err());
-                assert!(solver.execute("the one task").is_ok());
+                solver.execute("the_one_task").expect("could not execute");
                 
-                assert!(solver.get_value::<u32>("one").unwrap() == 1);
-                assert!(solver.get_value::<u32>("plusone").unwrap() == 2);
-                assert!(solver.get_value::<f32>("last_value").unwrap() == 3.0);
+                println!("{:?}", solver.get_values());
+
+                assert!(solver.get_value::<u32>("gen_one :: one").expect("never created?") == 1);
+                assert!(solver.get_value::<u32>("plus_one :: plusone").expect("never created?") == 2);
+                assert!(solver.get_value::<f32>("the_one_task :: last_value").expect("never created?") == 3.0);
             }
 
-            assert!(cache.get_value::<u32>("one").unwrap() == 1);
-            assert!(cache.get_value::<u32>("plusone").unwrap() == 2);
-            assert!(cache.get_value::<f32>("last_value").unwrap() == 3.0);
+            assert!(cache.get_value::<u32>("gen_one :: one").expect("not cached?") == 1);
+            assert!(cache.get_value::<u32>("plus_one :: plusone").expect("not cached?") == 2);
+            assert!(cache.get_value::<f32>("the_one_task :: last_value").expect("not cached?") == 3.0);
 
         }
 }
