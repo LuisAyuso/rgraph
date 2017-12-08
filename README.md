f163881ef94f5756565dc126dd0a6cc24531e0c6
Luis Ayuso <luis.f.ayuso@gmail.com>
2017-12-08 13:54:52 +0100

    add dot printer

--- README.md
+++ README.md
@@ -10,7 +10,8 @@ In order to satisfy the input of such task, all the producer tasks will be execu
 
 ## todo list
 
-- [ ] Hot swap: current task inputs and outputs have global visibility. Use a match table to bind inputs to outputs and allow rebind in runtime
-- [ ] Inter execution caching. allow a cache to survive the solver so it can be feed into another solver instance for the same graph. 
+- [x] Hot swap: current task inputs and outputs have global visibility. Use a match table to bind inputs to outputs and allow rebind in runtime
+- [x] Inter execution caching. allow a cache to survive the solver so it can be feed into another solver instance for the same graph. 
 - [ ] do not execute tasks with all dependencies satisfied.
+- [ ] do not execute tasks without input which result is known.
 - [ ] Add marker in inputs to choose the copy from the previous run instead of this run.
---
+++ src/printer.rs
@@ -0,0 +1,139 @@
+#![macro_use]
+
+use super::*;
+
+pub fn print_info(graph: &Graph) {
+    for (name, node) in graph.iter() {
+        print!("node: {} (", name);
+        for input in node.get_ins() {
+            print!("{},", input);
+        }
+        print!(") -> (");
+        for output in node.get_outs() {
+            print!("{},", output);
+        }
+        println!(")");
+    }
+}
+
+mod topo {
+
+    use super::*;
+
+    //use std::borrow::Cow;
+    //use std::io::Write;
+    use std::collections::BTreeMap as Map;
+    use std::vec::Vec;
+
+    type Nd<'a> = &'a str;
+    type Ed<'a> = (Nd<'a>, Nd<'a>, &'a str, &'a str);
+
+    impl<'a> dot::Labeller<'a, Nd<'a>, Ed<'a>> for Graph {
+        fn graph_id(&'a self) -> dot::Id<'a> {
+            dot::Id::new("example2").unwrap()
+        }
+        fn node_id(&'a self, n: &Nd) -> dot::Id<'a> {
+            dot::Id::new(format!("{}", n)).unwrap()
+        }
+        fn edge_label<'b>(&'b self, edge: &Ed) -> dot::LabelText<'b> {
+            let &(_, _, ref from, ref to) = edge;
+            dot::LabelText::LabelStr(format!("{} -> {}", to, from).into())
+        }
+    }
+
+
+    impl<'a> dot::GraphWalk<'a, Nd<'a>, Ed<'a>> for Graph {
+
+        fn nodes(&'a self) -> dot::Nodes<'a, Nd<'a>> {
+            self.iter().map(|s| s.0.as_str()).collect()
+        }
+
+        fn edges(&'a self) -> dot::Edges<'a, Ed<'a>> {
+
+            let mut ins : Map<&'a str,&'a str> = Map::new();
+            let mut out : Map<&'a str,&'a str> = Map::new();
+
+            let mut nodes : Vec<&'a str> = Vec::new();
+             
+            for (name, node) in self.iter() {
+                let name_str = name.as_str();
+                nodes.push(name_str);
+                for input in node.get_ins() {
+                    ins.insert(input.as_str(), name_str);
+                }
+                for output in node.get_outs() {
+                    out.insert(output.as_str(), name_str);
+                }
+            }
+            self.bindings.iter().map(|b|  {
+                    (*out.get(b.1.as_str()).expect("malformed graph"), 
+                     *ins.get(b.0.as_str()).expect("malformed graph"),
+                     b.1.as_str(), b.0.as_str()
+                     ) 
+                    }) .collect()
+
+
+        }
+
+        fn source(&self, e: &Ed<'a>) -> Nd<'a> {
+            let &(s, _, _, _) = e;
+            s
+        }
+
+        fn target(&self, e: &Ed<'a>) -> Nd<'a> {
+            let &(_, t, _, _) = e;
+            t
+        }
+    }
+
+}
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+#[cfg(test)]
+mod tests {
+
+    #![macro_use]
+    use super::*;
+
+    use std::io;
+
+    fn get_test_graph() -> Graph {
+        let mut g = Graph::new();
+        g.add_node(create_node!(sink_1 ( input : u32) -> ()
+                                 {
+                                     println!("sink 1 {}", input);
+                                 }));
+
+        g.add_node(create_node!(sink_2 ( name : u32) -> ()
+                                 {
+                                     println!("sink 2 {}", name);
+                                 }));
+
+        g.add_node(create_node!(no_input () -> ( i : u32)
+                                 {
+                                     i =  1234;
+                                     println!("produce {}", i);
+                                 }));
+        g
+    }
+    #[test]
+    fn info() {
+        let g = get_test_graph();
+        print_info(&g);
+    }
+
+    #[test]
+    fn dot() {
+        let g = get_test_graph();
+        dot::render(&g, &mut io::stdout()).expect("it should draw");
+    }
+    #[test]
+    fn dot2() {
+        let mut g = get_test_graph();
+        g.bind_asset("no_input :: i", "sink_1 :: input").expect("binding should exist");
+        g.bind_asset("no_input :: i", "sink_2 :: name").expect("binding should exist");
+        dot::render(&g, &mut io::stdout()).expect("it should draw");
+    }
+}
