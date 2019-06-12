81a741d793b79b6a56c9784fe097033b625d6159
LuisAyuso <luis.f.ayuso@gmail.com>
2019-06-12 20:47:22 +0200

    comment out bechmarks until i find out what to do with them

--- src/lib.rs
+++ src/lib.rs
@@ -120,10 +120,10 @@
 //! solver.execute_terminals().unwrap();
 //! ```
 
-#![feature(test)]
+// #![feature(test)]
 
 extern crate dot;
-extern crate test;
+// extern crate test;
 
 use std::any::Any;
 use std::cmp;
@@ -904,46 +904,45 @@ mod tests {
         assert!(g.get_unbound_assets().len() == 2);
     }
 
-    use test::Bencher;
+    // use test::Bencher;
+    // #[bench]
+    // fn benchmark_sequential(b: &mut Bencher) {
+    //     let mut g = Graph::new();
 
-    #[bench]
-    fn benchmark_sequential(b: &mut Bencher) {
-        let mut g = Graph::new();
+    //     let max = 1000;
 
-        let max = 1000;
-
-        // generate 10000 nodes
-        for i in 1..max {
-            let name: String = format!("task{}", i);
-            g.add_node(create_node!(name: name, ( input : u32) -> (output : u32)
-                                     { 
-                                         output = input +1 ;
-                                     }))
-                .unwrap();
-        }
+    //     // generate 10000 nodes
+    //     for i in 1..max {
+    //         let name: String = format!("task{}", i);
+    //         g.add_node(create_node!(name: name, ( input : u32) -> (output : u32)
+    //                                  { 
+    //                                      output = input +1 ;
+    //                                  }))
+    //             .unwrap();
+    //     }
 
 
-        // add sequential linking
-        for i in 1..max - 1 {
-            let src = format!("task{}::output", i);
-            let sink = format!("task{}::input", i + 1);
-            //println!("  {} -> {}", src, sink);
-            g.bind_asset(src.as_str(), sink.as_str())
-                .expect("binding must be doable");
-        }
+    //     // add sequential linking
+    //     for i in 1..max - 1 {
+    //         let src = format!("task{}::output", i);
+    //         let sink = format!("task{}::input", i + 1);
+    //         //println!("  {} -> {}", src, sink);
+    //         g.bind_asset(src.as_str(), sink.as_str())
+    //             .expect("binding must be doable");
+    //     }
 
-        g.define_freestanding_asset("start", 0u32).expect("could not create asset");
-            g.bind_asset("start", "task1::input")
-                .expect("could not bind first tast to start value");
+    //     g.define_freestanding_asset("start", 0u32).expect("could not create asset");
+    //         g.bind_asset("start", "task1::input")
+    //             .expect("could not bind first tast to start value");
 
-        // printer::print_info(&g);
+    //     // printer::print_info(&g);
 
-        b.iter(|| {
-            let mut cache = ValuesCache::new();
-            let mut solver = GraphSolver::new(&g, &mut cache);
+    //     b.iter(|| {
+    //         let mut cache = ValuesCache::new();
+    //         let mut solver = GraphSolver::new(&g, &mut cache);
 
-            let last_task = format!("task{}", max-1);
-            solver.execute(last_task.as_str()).expect("this should run");
-        });
-    }
+    //         let last_task = format!("task{}", max-1);
+    //         solver.execute(last_task.as_str()).expect("this should run");
+    //     });
+    // }
 }
